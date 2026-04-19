cef967bce802f6cfcf68e64d0e60397b7218adc9
WilliTourt <willitourt@foxmail.com>
2026-04-19 19:40:23 +0800

    updated lora functions

--- Software/EIDE Project folder/MambaQuad_V1/Components/Tasks/Inc/LoraTask.h
+++ Software/EIDE Project folder/MambaQuad_V1/Components/Tasks/Inc/LoraTask.h
@@ -30,38 +30,53 @@ class LoraTask : public FreeRTOS::Task {
             TRANSMIT,
             AT
         };
+        
+        enum class LoRaTransMode {
+            TRANSPARENT,
+            DIRECTIONAL,
+            BROADCAST
+        };
 
         enum class ATcmd {
-            AT,
-            RESET,
-            DEFAULT,
-            BAUD,
-            PARI,
-            HELP,
-            LEVEL,
-            MODE,
-            SLEEP,
-            SWITCH,
-            CHANNEL,
-            MAC,
-            OPENKEY,
-            KEY,
-            PACKET,
-            DRSSI,
-            POWE,
-            LBT,
-            LRSSI,
-            ERSSI
+            AT, RESET, DEFAULT, BAUD, PARI, LEVEL, MODE,
+            SLEEP, SWITCH, CHANNEL, MAC, OPENKEY, KEY, PACKET,
+            DRSSI, POWE, LBT, LRSSI
         };
 
         LoraTask(FreeRTOS::Queue<LoraMessage_t> &rxQueue,
                  LoraSerialTask &serial);
 
         bool init();
+        
+        /**
+         * @brief 配置 LoRa 模块参数
+         * @param channel 信道号 (0x00-0x63)
+         * @param level 速率等级 0-7 (0最慢最远, 7最快最近)
+         * @param mode 传输模式
+         * @param address 本机地址 (定点模式时需要)
+         * @param baud 波特率代码 (3=9600, 7=115200, 0=不修改)
+         * @return true 配置成功
+         */
+        bool configure(uint8_t channel = 0x00, 
+                       uint8_t level = 2, 
+                       LoRaTransMode mode = LoRaTransMode::TRANSPARENT,
+                       uint16_t address = 0x0001,
+                       uint8_t baud = 0);
 
-        void setMode(LoRaMode mode);
-        bool sendAT(ATcmd cmd, uint8_t *response = nullptr);
+        bool setMode(LoRaMode mode);
+        bool sendAT(ATcmd cmd, const char* arg = nullptr);
+        
+        /**
+         * @brief 发送数据
+         * @param data 数据指针
+         * @param len 数据长度
+         * @note 根据当前传输模式自动添加前缀
+         */
         void sendData(const uint8_t *data, size_t len);
+        
+        LoRaTransMode getTransMode() const { return _transMode; }
+        uint16_t getAddress() const { return _address; }
+        uint8_t getChannel() const { return _channel; }
 
     private:
         void taskFunction() override;
@@ -71,229 +86,8 @@ class LoraTask : public FreeRTOS::Task {
         LoraSerialTask &_serial;
 
         LoRaMode _mode;
+        LoRaTransMode _transMode;
+        
+        uint16_t _address;   // 本机地址 / 目标地址
+        uint8_t _channel;    // 当前信道
 };
-
-
-
-
-/* Lora
-
-// H
-
-class LoraTask : public FreeRTOS::Task {
-    public:
-
-        enum class LoRaMode {
-            TRANSMIT,
-            AT
-        };
-
-        enum class ATcmd {
-            AT,
-            RESET,
-            DEFAULT,
-            BAUD,
-            PARI,
-            HELP,
-            LEVEL,
-            MODE,
-            SLEEP,
-            SWITCH,
-            CHANNEL,
-            MAC,
-            OPENKEY,
-            KEY,
-            PACKET,
-            DRSSI,
-            POWE,
-            LBT,
-            LRSSI,
-            ERSSI
-        };
-
-
-        enum class LoRaTansmissionMode {
-            TRANSPARENT,    // 透传
-            DIRECTIONAL,     // 定向
-            BROADCAST        // 广播
-        };
-
-
-
-        LoraTask(FreeRTOS::Queue<LoraMessage_t> &rxQueue,
-                 LoraSerialTask &serial);
-
-        bool init();
-
-        void setMode(LoRaMode mode);
-        bool sendAT(ATcmd cmd, uint8_t *response = nullptr);
-        void sendData(const uint8_t *data, size_t len);
-
-    private:
-        void taskFunction() override;
-        const char* _getATstr(ATcmd cmd);
-
-        FreeRTOS::Queue<LoraMessage_t> &_fromLoraSerialQueue;
-        LoraSerialTask &_serial;
-
-        LoRaMode _mode;
-
-        uint16_t _address;
-        uint8_t _channel;
-};
-
-
-// cpp
-
-
-LoraTask::LoraTask(FreeRTOS::Queue<LoraMessage_t> &fromLoraSerialQueue, LoraSerialTask &serial) :
-                   Task(tskIDLE_PRIORITY + 2, 512, "LoRa"),
-                   _fromLoraSerialQueue(fromLoraSerialQueue),
-                   _serial(serial),
-                   _mode(LoRaMode::TRANSMIT) {}
-
-const char* LoraTask::_getATstr(ATcmd cmd) {
-    switch (cmd) {
-        case ATcmd::AT:         return "AT\r\n";
-        case ATcmd::RESET:      return "AT+RESET\r\n";
-        case ATcmd::DEFAULT:    return "AT+DEFAULT\r\n";
-        case ATcmd::BAUD:       return "AT+BAUD\r\n";
-        case ATcmd::PARI:       return "AT+PARI\r\n";
-        case ATcmd::HELP:       return "AT+HELP\r\n";
-        case ATcmd::LEVEL:      return "AT+LEVEL\r\n";
-        case ATcmd::MODE:       return "AT+MODE\r\n";
-        case ATcmd::SLEEP:      return "AT+SLEEP\r\n";
-        case ATcmd::SWITCH:     return "AT+SWITCH\r\n";
-        case ATcmd::CHANNEL:    return "AT+CHANNEL\r\n";
-        case ATcmd::MAC:        return "AT+MAC\r\n";
-        case ATcmd::OPENKEY:    return "AT+OPENKEY\r\n";
-        case ATcmd::KEY:        return "AT+KEY\r\n";
-        case ATcmd::PACKET:     return "AT+PACKET\r\n";
-        case ATcmd::DRSSI:      return "AT+DRSSI\r\n";
-        case ATcmd::POWE:       return "AT+POWE\r\n";
-        case ATcmd::LBT:        return "AT+LBT\r\n";
-        case ATcmd::LRSSI:      return "AT+LRSSI\r\n";
-        case ATcmd::ERSSI:      return "AT+ERSSI\r\n";
-        default:                return "ERR";
-    }
-}
-
-bool LoraTask::init() {
-    // placeholder initialization; nothing to configure at the moment
-    // could reset queues or send an AT command to probe the module
-    _mode = LoRaMode::TRANSMIT;
-    return true;
-}
-
-void LoraTask::setMode(LoRaMode mode) {
-    const char* enterCmd = "+++\r\n";
-    _serial.send((uint8_t*)enterCmd, (uint16_t)strlen(enterCmd));
-
-    _mode = mode;
-}
-
-bool LoraTask::sendAT(ATcmd cmd, uint8_t *response) {
-    const char* at = _getATstr(cmd);
-    if (at == nullptr || strcmp(at, "ERR") == 0) {
-        return false;
-    }
-
-    _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-    // response handling can be added later using `response` pointer
-    (void)response;
-    return true;
-}
-
-void LoraTask::sendData(const uint8_t *data, size_t len) {
-    // when in transmit mode send raw bytes to the radio
-    if(_mode != LoRaMode::TRANSMIT) 
-        return;
-
-    _serial.send((uint8_t*)data, (uint16_t)len);
-}
-
-
-
-
-/
- * mode: 0-透传，1-定向，2-广播
- * address: 定向模式下的目标地址，广播模式下无效
- * channel: 信道号
- * tansLevel: 传输等级，范围0-7，数值越大传输距离更短，但比特率更高
- * 
- /
-void LoraTask::set_transmissionMode(LoRaTansmissionMode mode, uint16_t address, uint8_t channel, uint8_t tansLevel) {
-
-    if(_mode != LoRaMode::AT) {
-        // Must be in AT mode to configure transmission mode
-        return;
-    }
-
-    setMode(LoRaMode::AT); // Ensure we're in AT mode to send configuration commands
-    char at[64];
-
-    _address = address;     // 需要保存下来,因为后续发送数据时需要知道目标地址
-    _channel = channel;
-
-    switch (mode) {
-        case LoRaTansmissionMode::TRANSPARENT:
-            // Configure for transparent transmission
-            sprintf(at, "AT+MODE=0\r\n");
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+LEVEL=%d\r\n", tansLevel);
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+CHANNEL=%d\r\n", channel);
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+RESET\r\n");
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-
-            break;
-        case LoRaTansmissionMode::DIRECTIONAL:
-            // Configure for directional transmission
-            sprintf(at, "AT+MODE=1\r\n");
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+LEVEL=%d\r\n", tansLevel);
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+RESET\r\n");
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-
-            break;
-        case LoRaTansmissionMode::BROADCAST:
-            // Configure for broadcast transmission
-            sprintf(at, "AT+MODE=2\r\n");
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+LEVEL=%d\r\n", tansLevel);
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-            sprintf(at, "AT+RESET\r\n");
-            _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-
-            break;
-    }
-
-    setMode(LoRaMode::AT);
-}
-
-
-
-void LoraTask::taskFunction() {
-    for (;;) {
-        auto opt = _fromLoraSerialQueue.receive(portMAX_DELAY);
-        if (!opt) {
-            continue;
-        }
-
-        // LoraMessage_t msg = *opt;
-
-        // msg.data contains a line-terminated sequence from the serial peripheral
-        if (_mode == LoRaMode::TRANSMIT) {
-            // when in transmit mode, received messages are packets coming in
-            // from the radio module.  dispatch them to whoever needs the data.
-            // e.g. push into another queue or invoke a callback.
-        } else {
-            // in AT mode the bytes are command responses; could be parsed
-            // by a state machine or stored in a buffer for later retrieval.
-        }
-    }
-}
-
-*/
--- Software/EIDE Project folder/MambaQuad_V1/Components/Tasks/Src/LoraTask.cpp
+++ Software/EIDE Project folder/MambaQuad_V1/Components/Tasks/Src/LoraTask.cpp
@@ -1,66 +1,175 @@
 #include "LoraTask.h"
+#include <cstring>
 
 LoraTask::LoraTask(FreeRTOS::Queue<LoraMessage_t> &fromLoraSerialQueue, LoraSerialTask &serial) :
                    Task(tskIDLE_PRIORITY + 2, 512, "LoRa"),
                    _fromLoraSerialQueue(fromLoraSerialQueue),
                    _serial(serial),
-                   _mode(LoRaMode::TRANSMIT) {}
+                   _mode(LoRaMode::TRANSMIT),
+                   _transMode(LoRaTransMode::TRANSPARENT),
+                   _address(0x0001),
+                   _channel(0x00) {}
 
 const char* LoraTask::_getATstr(ATcmd cmd) {
     switch (cmd) {
-        case ATcmd::AT:         return "AT\r\n";
-        case ATcmd::RESET:      return "AT+RESET\r\n";
-        case ATcmd::DEFAULT:    return "AT+DEFAULT\r\n";
-        case ATcmd::BAUD:       return "AT+BAUD\r\n";
-        case ATcmd::PARI:       return "AT+PARI\r\n";
-        case ATcmd::HELP:       return "AT+HELP\r\n";
-        case ATcmd::LEVEL:      return "AT+LEVEL\r\n";
-        case ATcmd::MODE:       return "AT+MODE\r\n";
-        case ATcmd::SLEEP:      return "AT+SLEEP\r\n";
-        case ATcmd::SWITCH:     return "AT+SWITCH\r\n";
-        case ATcmd::CHANNEL:    return "AT+CHANNEL\r\n";
-        case ATcmd::MAC:        return "AT+MAC\r\n";
-        case ATcmd::OPENKEY:    return "AT+OPENKEY\r\n";
-        case ATcmd::KEY:        return "AT+KEY\r\n";
-        case ATcmd::PACKET:     return "AT+PACKET\r\n";
-        case ATcmd::DRSSI:      return "AT+DRSSI\r\n";
-        case ATcmd::POWE:       return "AT+POWE\r\n";
-        case ATcmd::LBT:        return "AT+LBT\r\n";
-        case ATcmd::LRSSI:      return "AT+LRSSI\r\n";
-        case ATcmd::ERSSI:      return "AT+ERSSI\r\n";
-        default:                return "ERR";
+        case ATcmd::AT:         return "AT";
+        case ATcmd::RESET:      return "AT+RESET";
+        case ATcmd::DEFAULT:    return "AT+DEFAULT";
+        case ATcmd::BAUD:       return "AT+BAUD";
+        case ATcmd::PARI:       return "AT+PARI";
+        case ATcmd::LEVEL:      return "AT+LEVEL";
+        case ATcmd::MODE:       return "AT+MODE";
+        case ATcmd::SLEEP:      return "AT+SLEEP";
+        case ATcmd::SWITCH:     return "AT+SWITCH";
+        case ATcmd::CHANNEL:    return "AT+CHANNEL";
+        case ATcmd::MAC:        return "AT+MAC";
+        case ATcmd::OPENKEY:    return "AT+OPENKEY";
+        case ATcmd::KEY:        return "AT+KEY";
+        case ATcmd::PACKET:     return "AT+PACKET";
+        case ATcmd::DRSSI:      return "AT+DRSSI";
+        case ATcmd::POWE:       return "AT+POWE";
+        case ATcmd::LBT:        return "AT+LBT";
+        case ATcmd::LRSSI:      return "AT+LRSSI";
+        default:                return nullptr;
     }
 }
 
 bool LoraTask::init() {
-    // placeholder initialization; nothing to configure at the moment
-    // could reset queues or send an AT command to probe the module
     _mode = LoRaMode::TRANSMIT;
+    _transMode = LoRaTransMode::TRANSPARENT;
     return true;
 }
 
-void LoraTask::setMode(LoRaMode mode) {
-    const char* enterCmd = "+++\r\n";
-    _serial.send((uint8_t*)enterCmd, (uint16_t)strlen(enterCmd));
-
-    _mode = mode;
-}
-
-bool LoraTask::sendAT(ATcmd cmd, uint8_t *response) {
-    const char* at = _getATstr(cmd);
-    if (at == nullptr || strcmp(at, "ERR") == 0) {
+bool LoraTask::configure(uint8_t channel, uint8_t level, LoRaTransMode mode, 
+                         uint16_t address, uint8_t baud) {
+    // 保存配置
+    _channel = channel;
+    _address = address;
+    _transMode = mode;
+    
+    // 进入 AT 模式
+    if (!setMode(LoRaMode::AT)) {
         return false;
     }
+    
+    char arg[16];
+    bool ok = true;
+    
+    // baud rate
+    if (baud != 0) {
+        snprintf(arg, sizeof(arg), "%d", baud);
+        ok &= sendAT(ATcmd::BAUD, arg);
+        if (!ok) return false;
+    }
+    
+    // speed level, 0 farthest but slowest, 7 otherwise
+    snprintf(arg, sizeof(arg), "%d", level > 7 ? 7 : level);
+    ok &= sendAT(ATcmd::LEVEL, arg);
+    if (!ok) return false;
+    
+    // channel
+    snprintf(arg, sizeof(arg), "%02X", channel > 0x63 ? 0x63 : channel);
+    ok &= sendAT(ATcmd::CHANNEL, arg);
+    if (!ok) return false;
+    
+    // this device's addr
+    snprintf(arg, sizeof(arg), "%02X,%02X", (address >> 8) & 0xFF, address & 0xFF);
+    ok &= sendAT(ATcmd::MAC, arg);
+    if (!ok) return false;
+    
+    // trans mode (0 transparent, 1 directional, 2 broadcast)
+    snprintf(arg, sizeof(arg), "%d", static_cast<int>(mode));
+    ok &= sendAT(ATcmd::MODE, arg);
+    if (!ok) return false;
+    
+    ok &= sendAT(ATcmd::RESET, nullptr);
+    
+    return ok;
+}
 
-    _serial.send((uint8_t*)at, (uint16_t)strlen(at));
-    // response handling can be added later using `response` pointer
-    (void)response;
-    return true;
+bool LoraTask::setMode(LoRaMode mode) {
+    if (_mode == mode) return true;
+
+    while (_fromLoraSerialQueue.receive(0)) {}
+    this->delay(pdMS_TO_TICKS(100));
+
+    const char* cmd = "+++"; // no CRLF
+    _serial.send((uint8_t*)cmd, 3);
+
+    auto response = _fromLoraSerialQueue.receive(pdMS_TO_TICKS(500));
+    if (!response) return false;
+
+    if (mode == LoRaMode::AT) {
+        // "Entry AT"
+        if ((response->data[0] == 'E') && (response->data[1] == 'n')) {
+            _mode = LoRaMode::AT;
+            return true;
+        }
+    } else {
+        // "Exit AT"
+        if ((response->data[0] == 'E') && (response->data[1] == 'x')) {
+            this->delay(pdMS_TO_TICKS(500)); // wait for reboot
+            _mode = LoRaMode::TRANSMIT;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool LoraTask::sendAT(ATcmd cmd, const char* arg) {
+    if (_mode != LoRaMode::AT) return false;
+
+    char buffer[32];
+    const char* cmdPrefix = _getATstr(cmd);
+    if (cmdPrefix == nullptr) return false;
+
+    if (arg != nullptr) {
+        snprintf(buffer, sizeof(buffer), "%s%s\r\n", cmdPrefix, arg);
+    } else {
+        snprintf(buffer, sizeof(buffer), "%s\r\n", cmdPrefix);
+    }
+
+    _serial.send((uint8_t*)buffer, (uint16_t)strlen(buffer));
+
+    auto response = _fromLoraSerialQueue.receive(pdMS_TO_TICKS(500));
+    if (!response) return false;
+
+    if (strstr((char*)response->data, "OK") != nullptr) {
+        if (cmd == ATcmd::RESET) {
+            this->delay(pdMS_TO_TICKS(1000));
+            _mode = LoRaMode::TRANSMIT;
+        }
+        return true;
+    }
+
+    return false;
 }
 
 void LoraTask::sendData(const uint8_t *data, size_t len) {
-    // when in transmit mode send raw bytes to the radio
-    _serial.send((uint8_t*)data, (uint16_t)len);
+    if (_mode != LoRaMode::TRANSMIT) return;
+    
+    if (_transMode == LoRaTransMode::TRANSPARENT) { // [data]
+        _serial.send((uint8_t*)data, (uint16_t)len);
+    } else if (_transMode == LoRaTransMode::DIRECTIONAL) { // [addr2][chan1][data]
+        uint8_t packet[256];
+        if (len + 3 > sizeof(packet)) len = sizeof(packet) - 3;
+        
+        packet[0] = (_address >> 8) & 0xFF;
+        packet[1] = _address & 0xFF;
+        packet[2] = _channel;
+        memcpy(&packet[3], data, len);
+        
+        _serial.send(packet, (uint16_t)(len + 3));
+    } else if (_transMode == LoRaTransMode::BROADCAST) { // [chan1][data]
+        uint8_t packet[256];
+        if (len + 1 > sizeof(packet)) len = sizeof(packet) - 1;
+        
+        packet[0] = _channel;
+        memcpy(&packet[1], data, len);
+        
+        _serial.send(packet, (uint16_t)(len + 1));
+    }
 }
 
 void LoraTask::taskFunction() {
@@ -70,16 +179,18 @@ void LoraTask::taskFunction() {
             continue;
         }
 
-        // LoraMessage_t msg = *opt;
-
-        // msg.data contains a line-terminated sequence from the serial peripheral
         if (_mode == LoRaMode::TRANSMIT) {
-            // when in transmit mode, received messages are packets coming in
-            // from the radio module.  dispatch them to whoever needs the data.
-            // e.g. push into another queue or invoke a callback.
+            // 透传模式下收到的数据是空中传来的数据包
+            // 定点模式下收到的是：[源地址2字节][源信道1字节][数据]
+            // 广播模式下收到的是：[源信道1字节][数据]
+            
+            // TODO: 在这里处理接收到的数据
+            // 可以解析后发到飞控任务队列
+            // 比如：解析遥控指令、遥测数据等
+            
         } else {
-            // in AT mode the bytes are command responses; could be parsed
-            // by a state machine or stored in a buffer for later retrieval.
+            // AT 模式下收到的是命令响应
+            // 已经在 sendAT 里处理了，这里可以存日志或做状态机
         }
     }
 }
@@ -98,6 +209,7 @@ LoraSerialTask::LoraSerialTask(UART_HandleTypeDef *huart,
 void LoraSerialTask::rxProcess(uint8_t *rxData, uint16_t len) {
     LoraMessage_t msg;
     msg.length = len;
+    if (len > sizeof(msg.data)) len = sizeof(msg.data);
     memcpy(msg.data, rxData, len);
     msg.timestamp_ms = FreeRTOS::Kernel::getTickCount();
 
